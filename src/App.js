2fd4733c5cf80a7e391df67e46ccba0e7a5a8ddc
kwonhs <47519135+kwon1009@users.noreply.github.com>
2021-07-13 15:56:19 +0900

    jsx & props (#2.1)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 
+function Food({fav}) {
+  return <h1>I like {fav}</h1>;
+}
+
 function App() {
   return (
     <div className="App">Hello!
+      <Food fav="chicken" />
+      <Food fav="dessert" />
+      <Food fav="cookie" />
     </div>
   );
 }
